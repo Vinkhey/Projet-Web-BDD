453b0ffdaaaa55ee5f5d9e0fe39ca1b6f1c2282a
Romain Onrubia <romain.onrubia@cpnv.ch>
2019-05-20 10:18:16 +0200

    Create endLocation.php

---
+++ Sprint 2/view/endLocation.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * This php file is designed to manage all operation regarding cart's management
+ * Author   : Romain Onrubia
+ * Project  : Code
+ * Created  : 20.05.2019 - 09:30
+ *
+ * Last update :
+ */
+
+
+
+$title = 'Rent A Snow - Location effectuée';
+
+ob_start();
+?>
+    <h2>Votre panier</h2>
+    <article>
+        <form method="POST" action="index.php?action=displayLocation">
+            <table class="table">
+                <tr>
+                    <th>Code</th><th>Date</th><th>Nombre de jours</th><th>Quantité</th><th>Retirer</th>
+                </tr>
+                <?php
+                if(isset($_SESSION['CartErrors']))
+                {
+                    echo "Erreur sur la quantité demandée";
+                    echo "<br>".$_SESSION['CartErrors']."</br>";
+                    unset($_SESSION['CartErrors']);
+                }
+                // Displays cart session's content
+                $cartArray = $_SESSION['cart'];
+                foreach ($cartArray as $article){
+                    echo "<tr>";
+                    echo "<td>".$article['code']."</td>";
+                    echo "<td>".$article['dateD']."</td>";
+                    echo "<form method='POST' action='index.php?action=updateCartItem'>";
+                    echo "<td><input type='number' name='uQty' value='".$article['qty']."' disabled></td>";
+                    echo "<td><input type='number' name='uNbD' value='".$article['nbD']."' disabled></td>";
+
+                    echo "<td><a href='index.php?action=updateCartRequest&code=".$article['code']."'><img src='view/content/images/delete2.png'></a></td>";
+                    echo "</form></tr>";
+                }
+                ?>
+            </table>
+            <input type="submit" value="Louer encore" class="btn btn-success" name="backToCatalog">
+        </form>
+    </article>
+<?php
+$content = ob_get_clean();
+require 'gabarit.php';
+?>
