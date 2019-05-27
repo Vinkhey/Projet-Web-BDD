44037ab000a75eb92b111a52f8b8bcb298a787f3
Romain Onrubia <romain.onrubia@cpnv.ch>
2019-05-27 10:02:00 +0200

    Update gabarit.php

--- Sprint 2/view/gabarit.php
+++ Sprint 2/view/gabarit.php
@@ -84,11 +84,10 @@
                                 <?php else :?>
                                     <li><a href="index.php?action=logout">Logout</a></li>
                                 <?php endif; ?>
-                                <?php if(isset($_SESSION['cart']) || (!isset($_GET['action']))) :?>
+                                <?php if(isset($_SESSION['cart'])) :?>
                                     <li><a href="index.php?action=displayCart"><img src="view/content/images/cart.png">  <?=  count($_SESSION['cart']);?> snow(s)</a></li>
-                                <?php else: ?>
-                                <li><a href="index.php?action=endLocation"> Vos locations </a></li>
                                 <?php endif; ?>
+                                <li><a href="index.php?action=endLocation"> Vos locations </a></li>
                             </ul>
                             <!-- on affiche, si la session est active, l'adresse email de l'utilisateur-->
                             <?php if(isset($_SESSION['userEmailAddress'])) :?>
