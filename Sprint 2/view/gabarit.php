aeb5e56fcbdb00328689c855c028b781cefc6c03
Romain Onrubia <romain.onrubia@cpnv.ch>
2019-06-07 09:05:02 +0200

    petit changement

--- Sprint 2/view/gabarit.php
+++ Sprint 2/view/gabarit.php
@@ -88,7 +88,7 @@
                                     <li><a href="index.php?action=displayCart"><img src="view/content/images/cart.png">  <?=  count($_SESSION['cart']);?> snow(s)</a></li>
                                 <?php endif; ?>
                                 <li><a href="index.php?action=endLocation"> Vos locations </a></li>
-                                <li> <a href="index.php?action=managementLocation">Gestion des location</a></li>
+                                <li> <a href="index.php?action=managementLocation">Location</a></li>
                             </ul>
                             <!-- on affiche, si la session est active, l'adresse email de l'utilisateur-->
                             <?php if(isset($_SESSION['userEmailAddress'])) :?>
