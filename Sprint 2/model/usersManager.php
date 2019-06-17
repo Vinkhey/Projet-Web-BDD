cfdd6508c902af32b69c9ceb18ab93441627cfd7
Romain Onrubia <romain.onrubia@cpnv.ch>
2019-06-17 12:14:04 +0200

    Update usersManager.php

--- Sprint 2/model/usersManager.php
+++ Sprint 2/model/usersManager.php
@@ -54,8 +54,7 @@ function registerNewAccount($userEmailAddress, $userPsw){
         $userHashPsw = password_hash($userPsw, PASSWORD_DEFAULT);
 
         $registerQuery = 'INSERT INTO users (`userEmailAddress`, `userHashPsw`, `userType`) VALUES (' .$strSeparator . $userEmailAddress .$strSeparator . ','.$strSeparator . $userHashPsw .$strSeparator. ',1)';
-
-        echo ($registerQuery);
+        
         require_once 'model/dbConnector.php';
         $queryResult = executeQueryInsert($registerQuery);
         if($queryResult){
